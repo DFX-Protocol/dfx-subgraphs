92207ddb6e1ec62e39489a053a2ec4dda5322e4b
Bilal Ch <thered002@yahoo.com>
2023-07-27 22:16:26 +0500

    chore: initial commit

---
+++ config/arbitrum-one.js
@@ -0,0 +1,16 @@
+module.exports =
+  {
+    "network": "arbitrum-one",
+    "positionRouter": {
+      "address": "0x3D6bA331e3D9702C5e8A8d254e5d8a285F223aba",
+      "startBlock": "227000"
+    },
+    "rewardRouterV2": {
+        "address": "0xA906F338CB21815cBc4Bc87ace9e68c87eF8d8F1",
+        "startBlock": "227000"
+    },
+    "vault": {
+        "address": "0x489ee077994B6658eAfA855C308275EAd8097C4A",
+        "startBlock": "227000"
+    }
+  }
